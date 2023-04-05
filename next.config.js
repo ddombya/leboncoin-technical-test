1f8d609604ad1154d88eb6d36a21961b2129d333
ddombya <ddombya@student.42.fr>
2023-04-05 18:12:19 +0200

    deploy config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
   i18n: {
-    locales: ['fr'],
-    defaultLocale: 'fr',
+    locales: ["fr"],
+    defaultLocale: "fr",
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,4 +20,6 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
-}
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
